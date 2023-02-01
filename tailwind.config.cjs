c7c23dec189c884d3189d2b91478eeffbb99cfde
Adrian Zinko <adrian.zinko@boldare.com>
2023-02-01 22:07:06 +0100

    chore: config husky + typography :construction_worker:

--- tailwind.config.cjs
+++ tailwind.config.cjs
@@ -1,13 +1,13 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-  content: ["./src/**/*.{astro,html,js,jsx,md,mdx,ts,tsx}"],
+  content: ['./src/**/*.{astro,html,js,jsx,md,mdx,ts,tsx}'],
   theme: {
     screens: {
-      sm: "640px",
+      sm: '640px'
     },
     fontFamily: {
-      consola: ["Inconsolata", "monospace"],
-    },
+      consola: ['Inconsolata', 'monospace']
+    }
   },
-  plugins: [],
+  plugins: [require('@tailwindcss/typography')]
 };
