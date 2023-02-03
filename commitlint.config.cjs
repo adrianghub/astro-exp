9d1f9c84c71eaf0880723b22b818c44d96dd15cb
Adrian Zinko <adrian.zinko@boldare.com>
2023-02-03 12:58:01 +0100

    📝 Add commitlint configuration

---
+++ commitlint.config.cjs
@@ -0,0 +1,61 @@
+const matchAnyEmojiWithSpaceAfter =
+  /(\u00a9|\u00ae|[\u2000-\u3300]|\ud83c[\ud000-\udfff]|\ud83d[\ud000-\udfff]|\ud83e[\ud000-\udfff])\s/;
+const subjectThatDontStartWithBracket = /([^\[].+)/;
+
+module.exports = {
+  parserPreset: {
+    parserOpts: {
+      headerPattern: new RegExp(
+        "^" +
+          matchAnyEmojiWithSpaceAfter.source +
+          subjectThatDontStartWithBracket.source +
+          "$"
+      ),
+      headerCorrespondence: ["emoji", "subject"],
+    },
+  },
+  plugins: [
+    {
+      rules: {
+        "header-match-team-pattern": (parsed) => {
+          const { emoji, subject } = parsed;
+          if (emoji === null && subject === null) {
+            return [
+              false,
+              "header must be in format ✅ Add tests",
+            ];
+          }
+          return [true, ""];
+        },
+        "explained-emoji-enum": (parsed, _when, emojisObject) => {
+          const { emoji } = parsed;
+          if (emoji && !Object.keys(emojisObject).includes(emoji)) {
+            return [
+              false,
+              `emoji must be one of:
+${Object.keys(emojisObject)
+                .map((emojiType) => `${emojiType} - ${emojisObject[emojiType]}`)
+                .join("\n")}`,
+            ];
+          }
+          return [true, ""];
+        },
+      },
+    },
+  ],
+  rules: {
+    "header-match-team-pattern": [2, "always"],
+    "explained-emoji-enum": [
+      2,
+      "always",
+      {
+        "⭐️": "New feature",
+        "🐞": "Bug fix",
+        "✅": "Add, update tests",
+        "🚧": "Work in progress",
+        "♻️": "Refactor",
+        "📝": "Chore, update deps, config etc.",
+      },
+    ],
+  },
+};
